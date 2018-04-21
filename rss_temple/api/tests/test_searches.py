179bed1d49aa5f7f17413e7b60282e1c10e83cf5
Murray Christopherson <murraychristopherson@gmail.com>
2018-04-20 21:32:37 -0400

    Starting search tests

--- rss_temple/api/tests/test_searches.py
+++ rss_temple/api/tests/test_searches.py
@@ -3,4 +3,6 @@ from unittest import TestCase
 from api import searches
 
 class SearchesTestCase(TestCase):
-    pass
+    # TODO more tests
+    def test_standard(self):
+        searches.to_filter_args('feed', 'uuid:"99d63124-59e2-4204-ba61-be294dcb4d22|c54a1f76-f350-4336-b7c4-33ec8f5e81a3"')
