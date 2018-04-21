060135c8f2a88a931c8eda117cc0329d0735765a
Murray Christopherson <murraychristopherson@gmail.com>
2018-04-21 13:22:34 -0400

    Implementing authentication test

---
+++ rss_temple/api/tests/test_authenticate.py
@@ -0,0 +1,48 @@
+from unittest import TestCase
+import datetime
+import uuid
+
+from django.http import HttpRequest
+
+from api import authenticate, models
+
+class AuthenticateTestCase(TestCase):
+    def test_success(self):
+        user = None
+        try:
+            user = models.User.objects.get(email='test@test.com')
+        except models.User.DoesNotExist:
+            user = models.User()
+            user.email = 'test@test.com'
+
+            user.save()
+
+        session = models.Session()
+        session.user = user
+        session.expires_at = (datetime.datetime.utcnow() + datetime.timedelta(days=2))
+
+        session.save()
+
+        session_token = str(session.uuid)
+
+        request = HttpRequest()
+        request.META['HTTP_X_SESSION_TOKEN'] = session_token
+
+        self.assertTrue(authenticate.authenticate_http_request(request))
+
+    def test_fail(self):
+        request = HttpRequest()
+        request.META['HTTP_X_SESSION_TOKEN'] = str(uuid.uuid4())
+
+        self.assertFalse(authenticate.authenticate_http_request(request))
+
+    def test_no_token(self):
+        request = HttpRequest()
+
+        self.assertFalse(authenticate.authenticate_http_request(request))
+
+    def test_bad_session_token_format(self):
+        request = HttpRequest()
+        request.META['HTTP_X_SESSION_TOKEN'] = 'not a uuid'
+
+        self.assertFalse(authenticate.authenticate_http_request(request))
