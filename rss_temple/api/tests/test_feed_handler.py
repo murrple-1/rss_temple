0c798f87ea2599b428befa016ff2201e3766b8ee
Murray Christopherson <murraychristopherson@gmail.com>
2018-04-23 23:13:10 -0400

    Adding all feed types to the tests

--- rss_temple/api/tests/test_feed_handler.py
+++ rss_temple/api/tests/test_feed_handler.py
@@ -20,6 +20,14 @@ def _http_server_process():
         httpd.serve_forever()
 
 class FeedHandlerTestCase(TestCase):
+    FEED_TYPES = [
+        'atom_0.3',
+        'atom_1.0',
+        'rss_1.0',
+        'rss_2.0',
+        'rss_2.0_ns',
+    ]
+
     @classmethod
     def setUpClass(cls):
         logging.disable(logging.CRITICAL)
@@ -44,116 +52,65 @@ class FeedHandlerTestCase(TestCase):
         with self.assertRaises(QueryException):
             feed_handler.url_2_d('http://localhost:8080/rss_2.0/sample-404.xml')
 
-    def test_well_formed_rss_2_0(self):
-        text = None
-        with open('api/tests/test_files/rss_2.0/well_formed.xml', 'r') as f:
-            text = f.read()
+    def test_well_formed(self):
+        for feed_type in FeedHandlerTestCase.FEED_TYPES:
+            text = None
+            with open('api/tests/test_files/{}/well_formed.xml'.format(feed_type), 'r') as f:
+                text = f.read()
 
-        feed_handler.text_2_d(text)
-
-    def test_well_formed_atom_1_0(self):
-        text = None
-        with open('api/tests/test_files/atom_1.0/well_formed.xml', 'r') as f:
-            text = f.read()
-
-        feed_handler.text_2_d(text)
-
-    def test_malformed_rss_2_0(self):
-        text = None
-        with open('api/tests/test_files/rss_2.0/malformed.xml', 'r') as f:
-            text = f.read()
-
-        with self.assertRaises(QueryException):
             feed_handler.text_2_d(text)
 
-    def test_malformed_atom_1_0(self):
-        text = None
-        with open('api/tests/test_files/atom_1.0/malformed.xml', 'r') as f:
-            text = f.read()
+    def test_malformed(self):
+        for feed_type in FeedHandlerTestCase.FEED_TYPES:
+            text = None
+            with open('api/tests/test_files/{}/malformed.xml'.format(feed_type), 'r') as f:
+                text = f.read()
 
-        with self.assertRaises(QueryException):
-            feed_handler.text_2_d(text)
+            with self.assertRaises(QueryException):
+                feed_handler.text_2_d(text)
 
-    def test_d_feed_2_feed_rss_2_0(self):
-        text = None
-        with open('api/tests/test_files/rss_2.0/well_formed.xml', 'r') as f:
-            text = f.read()
+    def test_d_feed_2_feed(self):
+        for feed_type in FeedHandlerTestCase.FEED_TYPES:
+            text = None
+            with open('api/tests/test_files/{}/well_formed.xml'.format(feed_type), 'r') as f:
+                text = f.read()
 
-        d = feed_handler.text_2_d(text)
+            d = feed_handler.text_2_d(text)
 
-        url = 'http://www.example.com'
+            url = 'http://www.example.com'
 
-        feed = feed_handler.d_feed_2_feed(d.feed, url)
+            feed = feed_handler.d_feed_2_feed(d.feed, url)
 
-        self.assertEqual(feed.feed_url, url)
-        self.assertEqual(feed.title, d.feed.get('title'))
-        self.assertEqual(feed.home_url, d.feed.get('link'))
+            self.assertEqual(feed.feed_url, url)
+            self.assertEqual(feed.title, d.feed.get('title'))
+            self.assertEqual(feed.home_url, d.feed.get('link'))
 
-    def test_d_feed_2_feed_atom_1_0(self):
-        text = None
-        with open('api/tests/test_files/atom_1.0/well_formed.xml', 'r') as f:
-            text = f.read()
+    def test_d_feed_2_feed_entry(self):
+        for feed_type in FeedHandlerTestCase.FEED_TYPES:
+            text = None
+            with open('api/tests/test_files/{}/well_formed.xml'.format(feed_type), 'r') as f:
+                text = f.read()
 
-        d = feed_handler.text_2_d(text)
+            d = feed_handler.text_2_d(text)
 
-        url = 'http://www.example.com'
+            feed_entry = feed_handler.d_entry_2_feed_entry(d.entries[0])
 
-        feed = feed_handler.d_feed_2_feed(d.feed, url)
+    def test_d_feed_2_feed_tags(self):
+        for feed_type in FeedHandlerTestCase.FEED_TYPES:
+            text = None
+            with open('api/tests/test_files/{}/well_formed.xml'.format(feed_type), 'r') as f:
+                text = f.read()
 
-        self.assertEqual(feed.feed_url, url)
-        self.assertEqual(feed.title, d.feed.get('title'))
-        self.assertEqual(feed.home_url, d.feed.get('link'))
+            d = feed_handler.text_2_d(text)
 
-    def test_d_feed_2_feed_entry_rss_2_0(self):
-        text = None
-        with open('api/tests/test_files/rss_2.0/well_formed.xml', 'r') as f:
-            text = f.read()
+            feed_tags = feed_handler.d_feed_2_feed_tags(d.feed)
 
-        d = feed_handler.text_2_d(text)
+    def test_d_entry_2_entry_tags(self):
+        for feed_type in FeedHandlerTestCase.FEED_TYPES:
+            text = None
+            with open('api/tests/test_files/{}/well_formed.xml'.format(feed_type), 'r') as f:
+                text = f.read()
 
-        feed_entry = feed_handler.d_entry_2_feed_entry(d.entries[0])
+            d = feed_handler.text_2_d(text)
 
-    def test_d_feed_2_feed_entry_atom_1_0(self):
-        text = None
-        with open('api/tests/test_files/atom_1.0/well_formed.xml', 'r') as f:
-            text = f.read()
-
-        d = feed_handler.text_2_d(text)
-
-        feed_entry = feed_handler.d_entry_2_feed_entry(d.entries[0])
-
-    def test_d_feed_2_feed_tags_rss_2_0(self):
-        text = None
-        with open('api/tests/test_files/rss_2.0/well_formed.xml', 'r') as f:
-            text = f.read()
-
-        d = feed_handler.text_2_d(text)
-
-        feed_tags = feed_handler.d_feed_2_feed_tags(d.feed)
-
-    def test_d_feed_2_feed_tags_atom_1_0(self):
-        text = None
-        with open('api/tests/test_files/atom_1.0/well_formed.xml', 'r') as f:
-            text = f.read()
-
-        d = feed_handler.text_2_d(text)
-
-        feed_tags = feed_handler.d_feed_2_feed_tags(d.feed)
-
-    def test_d_entry_2_entry_tags_rss_2_0(self):
-        text = None
-        with open('api/tests/test_files/rss_2.0/well_formed.xml', 'r') as f:
-            text = f.read()
-
-        d = feed_handler.text_2_d(text)
-
-        entry_tags = feed_handler.d_entry_2_entry_tags(d.entries[0])
-
-    def test_d_entry_2_entry_tags_atom_1_0(self):
-        text = None
-        with open('api/tests/test_files/atom_1.0/well_formed.xml', 'r') as f:
-            text = f.read()
-
-        d = feed_handler.text_2_d(text)
-
-        entry_tags = feed_handler.d_entry_2_entry_tags(d.entries[0])
+            entry_tags = feed_handler.d_entry_2_entry_tags(d.entries[0])
