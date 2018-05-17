ccccd9062e1ada48d3ef03018a419f539897da86
Murray Christopherson <murraychristopherson@gmail.com>
2018-05-16 21:21:28 -0400

    More searchqueries testing

--- rss_temple/api/tests/test_searchqueries.py
+++ rss_temple/api/tests/test_searchqueries.py
@@ -5,6 +5,7 @@ from decimal import Decimal
 from django.http.request import QueryDict
 
 from api import searchqueries
+from api.exceptions import QueryException
 
 
 class SearchQueriesTestCase(TestCase):
@@ -33,3 +34,38 @@ class SearchQueriesTestCase(TestCase):
 
         self.assertEquals(content_type, 'application/json')
         self.assertEquals(content, '{"test1":1,"test2":true}')
+
+    def test_get_count(self):
+        self.assertEquals(searchqueries.get_count(QueryDict('count=5')), 5)
+
+        with self.assertRaises(QueryException):
+            searchqueries.get_count(QueryDict('count=-1'))
+
+        with self.assertRaises(QueryException):
+            searchqueries.get_count(QueryDict('count=1001'))
+
+        with self.assertRaises(QueryException):
+            searchqueries.get_count(QueryDict('count=test'))
+
+        self.assertEquals(searchqueries.get_count(QueryDict()), searchqueries._DEFAULT_COUNT)
+
+        self.assertEquals(searchqueries.get_count(QueryDict('tcount=5'), param_name='tcount'), 5)
+
+        searchqueries.get_count(QueryDict('count=1001'), max=1050)
+
+        self.assertEquals(searchqueries.get_count(QueryDict(''), default=5), 5)
+
+    def test_get_skip(self):
+        self.assertEquals(searchqueries.get_skip(QueryDict('skip=5')), 5)
+
+        with self.assertRaises(QueryException):
+            searchqueries.get_skip(QueryDict('skip=-1'))
+
+        with self.assertRaises(QueryException):
+            searchqueries.get_skip(QueryDict('skip=test'))
+
+        self.assertEquals(searchqueries.get_skip(QueryDict('tskip=5'), param_name='tskip'), 5)
+
+        self.assertEquals(searchqueries.get_skip(QueryDict('')), searchqueries._DEFAULT_SKIP)
+
+        self.assertEquals(searchqueries.get_skip(QueryDict(''), default= 5), 5)
