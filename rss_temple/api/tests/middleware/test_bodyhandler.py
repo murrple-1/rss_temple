163078990085642dc02293214989cff7b3128442
Murray Christopherson <murraychristopherson@gmail.com>
2018-05-20 18:56:15 -0400

    Bodyhandler test

--- rss_temple/api/tests/middleware/test_bodyhandler.py
+++ rss_temple/api/tests/middleware/test_bodyhandler.py
@@ -1,7 +1,16 @@
 from django.test import TestCase
+from django.http.response import HttpResponse
 
 import api.middleware.bodyhandler as bodyhandler
 
 class BodyHandlerTestCase(TestCase):
     def test_middleware(self):
-        pass
+        middleware = bodyhandler.BodyHandlerMiddleware(lambda request: HttpResponse())
+
+        class MockHttpRequest:
+            def __init__(self):
+                self.body = None
+
+        request = MockHttpRequest()
+
+        response = middleware(request)
