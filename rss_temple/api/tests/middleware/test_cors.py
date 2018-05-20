dd25177b32b1f34e69110fcc3e0d9c3baeffa43b
Murray Christopherson <murraychristopherson@gmail.com>
2018-05-20 18:53:11 -0400

    CORS middleware tests

--- rss_temple/api/tests/middleware/test_cors.py
+++ rss_temple/api/tests/middleware/test_cors.py
@@ -1,7 +1,20 @@
+import importlib
+
 from django.test import TestCase
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
 
 import api.middleware.cors as cors
 
 class CORSTestCase(TestCase):
     def test_middleware(self):
-        pass
+        with self.settings(CORS_ALLOW_ORIGINS='*',
+            CORS_ALLOW_METHODS='GET,POST,OPTIONS',
+            CORS_ALLOW_HEADERS='Pragma,Cache-Control,Content-Type,If-Modified-Since,Authorization',
+            CORS_EXPOSE_HEADERS=''):
+            importlib.reload(cors)
+            middleware = cors.CORSMiddleware(lambda request: HttpResponse())
+
+            request = HttpRequest()
+
+            response = middleware(request)
