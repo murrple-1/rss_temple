c46013420452908134f3f84a776c17dd73b4a2e7
Murray Christopherson <murraychristopherson@gmail.com>
2018-05-21 21:27:18 -0400

    Splitting the session_cleanup_daemon for easier testing

---
+++ rss_temple/api/session_cleanup_daemon/__init__.py
@@ -0,0 +1,47 @@
+import logging
+import sys
+import time
+import argparse
+
+from django.db.models.functions import Now
+
+import filelock
+
+from api import models
+
+_logger = None
+def logger():
+    global _logger
+
+    if _logger is None:
+        _logger = logging.getLogger(__name__)
+        _logger.setLevel(logging.DEBUG)
+
+        stream_handler = logging.StreamHandler(sys.stdout)
+        stream_handler.setLevel(logging.DEBUG)
+        stream_handler.setFormatter(
+            logging.Formatter(
+                fmt='%(asctime)s (%(levelname)s): %(message)s',
+                datefmt='%Y-%m-%d %H:%M:%S'))
+        _logger.addHandler(stream_handler)
+
+        file_handler = logging.handlers.RotatingFileHandler(
+            filename='session_cleanup_daemon.log', maxBytes=(
+                50 * 100000), backupCount=3)
+        file_handler.setLevel(logging.WARNING)
+        file_handler.setFormatter(
+            logging.Formatter(
+                fmt='%(asctime)s (%(levelname)s): %(message)s',
+                datefmt='%Y-%m-%d %H:%M:%S'))
+        _logger.addHandler(file_handler)
+
+    return _logger
+
+
+def cleanup():
+    count, _ = models.Session.objects.filter(expires_at__lt=Now()).delete()
+
+    if count > 0:
+        logger().debug('deleting %s sessions', count)
+    else:
+        logger().debug('no sessions deleted')
