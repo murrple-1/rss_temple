dab4f551929b51f72ce829b21047df0a6629765d
Murray Christopherson <murraychristopherson@gmail.com>
2018-05-21 22:04:44 -0400

    Cleaning up coverage usage

--- rss_temple/api/session_cleanup_daemon/__main__.py
+++ rss_temple/api/session_cleanup_daemon/__main__.py
@@ -14,26 +14,30 @@ django.setup()
 
 from . import cleanup, logger
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    '-f',
-    '--force-now',
-    help='Clear the expired sessions immediately, then exit',
-    action='store_true',
-    dest='runNow')
-args = parser.parse_args()
 
-if not args.runNow:
-    lock = filelock.FileLock('session_cleanup_daemon.lock')
-    try:
-        with lock.acquire(timeout=1):
-            while True:
-                cleanup()
-                time.sleep(60 * 60 * 24)
-    except filelock.Timeout:
-        logger().info('only 1 process allowed at a time - lock file already held')
-    except Exception:
-        logger().exception('cleanup loop stopped unexpectedly')
-        raise
-else:
-    cleanup()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-f',
+        '--force-now',
+        help='Clear the expired sessions immediately, then exit',
+        action='store_true',
+        dest='runNow')
+    args = parser.parse_args()
+
+    if not args.runNow:
+        lock = filelock.FileLock('session_cleanup_daemon.lock')
+        try:
+            with lock.acquire(timeout=1):
+                while True:
+                    cleanup()
+                    time.sleep(60 * 60 * 24)
+        except filelock.Timeout:
+            logger().info('only 1 process allowed at a time - lock file already held')
+        except Exception:
+            logger().exception('cleanup loop stopped unexpectedly')
+            raise
+    else:
+        cleanup()
+
+main()
