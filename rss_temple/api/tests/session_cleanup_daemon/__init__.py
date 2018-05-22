18e156bd111c637994a7adeeac08b56c81d798ad
Murray Christopherson <murraychristopherson@gmail.com>
2018-05-21 22:35:51 -0400

    Adding test cases for the session_cleanup_daemon

---
+++ rss_temple/api/tests/session_cleanup_daemon/__init__.py
@@ -0,0 +1,50 @@
+import datetime
+import logging
+
+from django.test import TestCase
+
+import api.models as models
+from api.session_cleanup_daemon import cleanup
+
+
+class DaemonTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        logging.disable(logging.CRITICAL)
+
+        cls.user = models.User(email='example@example.com')
+        cls.user.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        logging.disable(logging.NOTSET)
+
+        cls.user.delete()
+
+    def test_none(self):
+        user = DaemonTestCase.user
+
+        models.Session.objects.filter(user=user).delete()
+
+        self.assertEquals(models.Session.objects.filter(user=user).count(), 0)
+
+        cleanup()
+
+        self.assertEquals(models.Session.objects.filter(user=user).count(), 0)
+
+    def test_some(self):
+        user = DaemonTestCase.user
+
+        models.Session.objects.filter(user=user).delete()
+
+        self.assertEquals(models.Session.objects.filter(user=user).count(), 0)
+
+        models.Session(expires_at=datetime.datetime.utcnow() + datetime.timedelta(days=-1), user=user).save()
+
+        models.Session(expires_at=datetime.datetime.utcnow() + datetime.timedelta(days=1), user=user).save()
+
+        self.assertEquals(models.Session.objects.filter(user=user).count(), 2)
+
+        cleanup()
+
+        self.assertEquals(models.Session.objects.filter(user=user).count(), 1)
