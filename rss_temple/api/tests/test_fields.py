481d8c0fef35cf5a59d0a795b139ff24ce9c9129
Murray Christopherson <murraychristopherson@gmail.com>
2018-10-18 19:52:58 -0400

    fixing tests

--- rss_temple/api/tests/test_fields.py
+++ rss_temple/api/tests/test_fields.py
@@ -5,28 +5,45 @@ from django.test import TestCase
 from api import fields
 
 
+class _ObjectConfig:
+    def __init__(self, object_name, all_count, default_count, good_field_name, bad_field_name='bad_field_name'):
+        self.object_name = object_name
+        self.all_count = all_count
+        self.default_count = default_count
+        self.good_field_name = good_field_name
+        self.bad_field_name = bad_field_name
+
+
+_object_configs = [
+    _ObjectConfig('user', 3, 1, 'uuid'),
+    _ObjectConfig('feed', 7, 1, 'uuid'),
+    _ObjectConfig('feedentry', 10, 1, 'uuid'),
+]
+
 class FieldsTestCase(TestCase):
     def test_default_field_maps(self):
-        default_field_maps = fields.get_default_field_maps('feed')
+        for oc in _object_configs:
+            default_field_maps = fields.get_default_field_maps(oc.object_name)
 
-        self.assertEquals(len(default_field_maps), 1)
+            self.assertEquals(len(default_field_maps), oc.default_count)
 
-        for field_map in default_field_maps:
-            self.assertIn('field_name', field_map)
-            self.assertIsInstance(field_map['field_name'], str)
-            self.assertIn('accessor', field_map)
-            self.assertTrue(callable(field_map['accessor']))
+            for field_map in default_field_maps:
+                self.assertIn('field_name', field_map)
+                self.assertIsInstance(field_map['field_name'], str)
+                self.assertIn('accessor', field_map)
+                self.assertTrue(callable(field_map['accessor']))
 
     def test_all_field_maps(self):
-        all_field_maps = fields.get_all_field_maps('feed')
+        for oc in _object_configs:
+            all_field_maps = fields.get_all_field_maps(oc.object_name)
 
-        self.assertEquals(len(all_field_maps), 6)
+            self.assertEquals(len(all_field_maps), oc.all_count)
 
-        for field_map in all_field_maps:
-            self.assertIn('field_name', field_map)
-            self.assertIsInstance(field_map['field_name'], str)
-            self.assertIn('accessor', field_map)
-            self.assertTrue(callable(field_map['accessor']))
+            for field_map in all_field_maps:
+                self.assertIn('field_name', field_map)
+                self.assertIsInstance(field_map['field_name'], str)
+                self.assertIn('accessor', field_map)
+                self.assertTrue(callable(field_map['accessor']))
 
     def test_fieldconfig(self):
         fc = fields._FieldConfig(lambda context, db_obj: 'test', True)
@@ -41,21 +58,22 @@ class FieldsTestCase(TestCase):
             fields._FieldConfig(lambda: None, None)
 
     def test_to_field_map(self):
-        field_map = fields.to_field_map('feed', 'uuid')
+        for oc in _object_configs:
+            field_map = fields.to_field_map(oc.object_name, oc.good_field_name)
 
-        self.assertEquals(field_map['field_name'], 'uuid')
+            self.assertEquals(field_map['field_name'], oc.good_field_name)
 
-        class TestContext:
-            pass
+            class TestContext:
+                pass
 
-        class TestObject:
-            def __init__(self):
-                self.uuid = uuid.uuid4()
+            class TestObject:
+                def __init__(self):
+                    self.uuid = uuid.uuid4()
 
-        test_context = TestContext()
-        test_obj = TestObject()
-        self.assertIsInstance(field_map['accessor'](
-            test_context, test_obj), str)
+            test_context = TestContext()
+            test_obj = TestObject()
+            self.assertIsInstance(field_map['accessor'](
+                test_context, test_obj), str)
 
-        field_map = fields.to_field_map('feed', 'bad_field_name')
-        self.assertIsNone(field_map)
+            field_map = fields.to_field_map(oc.object_name, oc.bad_field_name)
+            self.assertIsNone(field_map)
