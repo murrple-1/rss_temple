68088f5517d3bd40114a6cde2d6fadd4f24c41c0
Murray Christopherson <murraychristopherson@gmail.com>
2018-11-20 14:09:32 -0500

    making it more obvious that various XML libraries are being used

--- rss_temple/api/views/opml.py
+++ rss_temple/api/views/opml.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotFou
 from django.db.utils import IntegrityError
 from django.db import transaction
 
-from lxml import etree
+from lxml import etree as lxml_etree
 
 from defusedxml.ElementTree import fromstring as defused_fromstring, ParseError as defused_ParseError
 
@@ -28,23 +28,23 @@ def opml(request):
 def _opml_get(request):
     category_dict = request.user.category_dict()
 
-    opml_element = etree.Element('opml', version='1.0')
-    head_element = etree.SubElement(opml_element, 'head')
-    title_element = etree.SubElement(head_element, 'title')
+    opml_element = lxml_etree.Element('opml', version='1.0')
+    head_element = lxml_etree.SubElement(opml_element, 'head')
+    title_element = lxml_etree.SubElement(head_element, 'title')
     title_element.text = 'RSS Temple OMPL'
-    body_element = etree.SubElement(opml_element, 'body')
+    body_element = lxml_etree.SubElement(opml_element, 'body')
 
     for key, feeds in category_dict.items():
         outer_outline_name = key if key is not None else 'Not Categorized'
 
-        outer_outline_element = etree.SubElement(body_element, 'outline', text=outer_outline_name, title=outer_outline_name)
+        outer_outline_element = lxml_etree.SubElement(body_element, 'outline', text=outer_outline_name, title=outer_outline_name)
 
         for feed in feeds:
             outline_name = feed._custom_title if feed._custom_title is not None else feed.title
-            etree.SubElement(outer_outline_element, 'outline',
+            lxml_etree.SubElement(outer_outline_element, 'outline',
                 type='rss', text=outline_name, title=outline_name, xmlUrl=feed.feed_url, htmlUrl=feed.home_url)
 
-    return HttpResponse(etree.tostring(opml_element), 'text/xml')
+    return HttpResponse(lxml_etree.tostring(opml_element), 'text/xml')
 
 
 def _opml_post(request):
