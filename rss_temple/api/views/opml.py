98a48ff94a1174d90996b910a72915c1cdb542e9
Murray Christopherson <murraychristopherson@gmail.com>
2018-11-20 20:40:46 -0500

    completed first pass at reading in OPML

--- rss_temple/api/views/opml.py
+++ rss_temple/api/views/opml.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotFound, HttpResponseNotAllowed
-from django.db.utils import IntegrityError
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotAllowed
 from django.db import transaction
 
 from lxml import etree as lxml_etree
@@ -8,9 +7,8 @@ from defusedxml.ElementTree import fromstring as defused_fromstring, ParseError 
 
 import xmlschema
 
-from api import models, searchqueries, feed_handler, opml as opml_util
+from api import models, feed_handler, opml as opml_util
 from api.exceptions import QueryException
-from api.context import Context
 
 
 def opml(request):
@@ -23,6 +21,18 @@ def opml(request):
         return _opml_get(request)
     elif request.method == 'POST':
         return _opml_post(request)
+
+
+def _generate_feed(url):
+    d = feed_handler.url_2_d(url)
+    feed = feed_handler.d_feed_2_feed(d.feed, url)
+    feed._feed_entries = []
+
+    for d_entry in d.get('entries', []):
+        feed_entry = feed_handler.d_entry_2_feed_entry(d_entry)
+        feed._feed_entries.append(feed_entry)
+
+    return feed
 
 
 def _opml_get(request):
@@ -48,6 +58,8 @@ def _opml_get(request):
 
 
 def _opml_post(request):
+    user = request.user
+
     if not request.body:
         return HttpResponseBadRequest('no HTTP body')  # pragma: no cover
 
@@ -62,16 +74,72 @@ def _opml_post(request):
     except xmlschema.XMLSchemaException:
         return HttpResponseBadRequest('OPML not valid')
 
+    user_categories = []
+
+    rss_detail_tuples = set()
     for outer_outline_element in opml_element.findall('./body/outline'):
         outer_outline_name = outer_outline_element.attrib['title']
 
+        user_category = None
+        try:
+            user_category = models.UserCategory.objects.get(user=user, text=outer_outline_name)
+            user_category._is_new = False
+        except models.UserCategory.DoesNotExist:
+            user_category = models.UserCategory(user=user, text=outer_outline_name)
+            user_category._is_new = True
+
+        inner_rss_detail_tuples = set()
+
         for outline_element in outer_outline_element.findall('./outline'):
             outline_name = outline_element.attrib['title']
-
             outline_xml_url = outline_element.attrib['xmlUrl']
 
-            # TODO build feeds
+            inner_rss_detail_tuples.add((outline_name, outline_xml_url))
 
-    # TODO finish
+        user_category._feeds = []
+
+        for name, xml_url in inner_rss_detail_tuples.difference(rss_detail_tuples):
+            if models.SubscribedFeedUserMapping.objects.filter(user=user, feed__feed_url=xml_url).exists():
+                continue
+
+            feed = None
+            try:
+                feed = models.Feed.objects.get(feed_url=xml_url)
+                feed._is_new = False
+            except models.Feed.DoesNotExist:
+                try:
+                    feed = _generate_feed(xml_url)
+                except QueryException as e:
+                    return HttpResponse(e.message, status=e.httpcode)
+                feed._is_new = True
+
+            feed._custom_title_ = name if name != feed.title else None
+
+            user_category._feeds.append(feed)
+
+        user_categories.append(user_category)
+
+        rss_detail_tuples.update(inner_rss_detail_tuples)
+
+    subscribed_feed_user_mappings = []
+    with transaction.atomic():
+        for user_category in user_categories:
+            if user_category._is_new:
+                user_category.save()
+
+            for feed in user_category._feeds:
+                if feed._is_new:
+                    feed.save()
+
+                    for feed_entry in feed._feed_entries:
+                        feed_entry.feed = feed
+
+                    models.FeedEntry.objects.bulk_create(feed._feed_entries)
+
+                subscribed_feed_user_mapping = models.SubscribedFeedUserMapping(
+                    feed=feed, user=user, custom_feed_title=feed._custom_title_, user_category=user_category)
+                subscribed_feed_user_mappings.append(subscribed_feed_user_mapping)
+
+        models.SubscribedFeedUserMapping.objects.bulk_create(subscribed_feed_user_mappings)
 
     return HttpResponse()
