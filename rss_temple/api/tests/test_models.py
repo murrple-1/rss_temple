7ff7f5e0d79e46ef87a47ca2aa6f6fcb57a9c6b8
U-DESKTOP-8LJHFQ9\murrp <murraychristopherson@gmail.com>
2018-12-12 09:34:10 -0500

    add a quick test for the feed entry eq method

---
+++ rss_temple/api/tests/test_models.py
@@ -0,0 +1,43 @@
+import datetime
+
+from django.test import TestCase
+
+from api import models
+
+class FeedEntryTestCase(TestCase):
+    def test_eq(self):
+        feed = models.Feed(
+            feed_url='http://example.com/rss.xml',
+            title='Sample Feed',
+            home_url='http://example.com',
+            published_at=datetime.datetime.utcnow(),
+            updated_at=None,
+            db_updated_at=None)
+
+        feed_entry1 = models.FeedEntry(
+            id=None,
+            feed=feed,
+            created_at=None,
+            updated_at=None,
+            title='Feed Entry 1 Title',
+            url='http://example.com/entry1.html',
+            content='Some Entry content',
+            author_name='John Doe',
+            db_updated_at=None)
+
+        feed_entry2 = models.FeedEntry(
+            id=None,
+            feed=feed,
+            created_at=None,
+            updated_at=None,
+            title='Feed Entry 2 Title',
+            url='http://example.com/entry2.html',
+            content='Some Entry content',
+            author_name='John Doe',
+            db_updated_at=None)
+
+        self.assertTrue(feed_entry1 == feed_entry1)
+
+        self.assertFalse(feed_entry1 == feed_entry2)
+
+        self.assertFalse(feed_entry1 == object())
