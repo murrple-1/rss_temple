b9d0e12b38a4a36a0c99eb78211440cc950638b5
Murray Christopherson <murraychristopherson@gmail.com>
2020-04-22 22:04:23 -0400

    minor test cleanup

--- rss_temple/api/tests/third_party_login/test_facebook.py
+++ rss_temple/api/tests/third_party_login/test_facebook.py
@@ -6,7 +6,7 @@ from django.http import HttpRequest
 
 from api.third_party_login import facebook
 
-@unittest.skipIf(not {'TEST_REMOTE', 'TEST_FACEBOOK_TOKEN'}.issubset(frozenset(os.environ)), '`TEST_REMOTE`, and `TEST_FACEBOOK_TOKEN` env vars must be set')
+@unittest.skipIf(not {'TEST_REMOTE', 'TEST_FACEBOOK_TOKEN'}.issubset(frozenset(os.environ)), '`TEST_REMOTE`, and `TEST_FACEBOOK_TOKEN` env var(s) must be set: remote test')
 class FacebookTestCase(TestCase):
     def test_get_id(self):
         profile_id = facebook.get_id(os.environ['TEST_FACEBOOK_TOKEN'])
