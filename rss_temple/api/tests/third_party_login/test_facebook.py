80338a219e15d5ce5db9e6fcda8ebe7608328ce3
Murray Christopherson <murraychristopherson@gmail.com>
2020-04-22 20:45:52 -0400

    adding FB tests

---
+++ rss_temple/api/tests/third_party_login/test_facebook.py
@@ -0,0 +1,26 @@
+import unittest
+import os
+
+from django.test import TestCase
+from django.http import HttpRequest
+
+from api.third_party_login import facebook
+
+@unittest.skipIf(not {'TEST_REMOTE', 'TEST_FACEBOOK_TOKEN'}.issubset(frozenset(os.environ)), '`TEST_REMOTE`, and `TEST_FACEBOOK_TOKEN` env vars must be set')
+class FacebookTestCase(TestCase):
+    def test_get_id(self):
+        profile_id = facebook.get_id(os.environ['TEST_FACEBOOK_TOKEN'])
+        self.assertIsInstance(profile_id, str)
+
+    def test_get_id_badtoken(self):
+        with self.assertRaises(ValueError):
+            facebook.get_id('badtoken')
+
+    def test_get_id_and_email(self):
+        profile_id, email = facebook.get_id_and_email(os.environ['TEST_FACEBOOK_TOKEN'])
+        self.assertIsInstance(profile_id, str)
+        self.assertTrue(email is None or type(email) is str)
+
+    def test_get_id_and_email_badtoken(self):
+        with self.assertRaises(ValueError):
+            facebook.get_id_and_email('badtoken')
