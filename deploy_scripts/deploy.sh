f2ee55357cdf7a6dc761a5a9bb9139a619865799
Murray Christopherson <murraychristopherson@gmail.com>
2020-12-14 22:50:44 -0800

    first pass at deploy script

--- deploy_scripts/deploy.sh
+++ deploy_scripts/deploy.sh
@@ -1,23 +1,31 @@
 #!/usr/bin/env sh
 
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 [latest,<build_number>]"
+    echo "Usage: $0 [latest,<job_number>]"
     exit
 fi
 
-BUILD_NUMBER=$1
+JOB_NUMBER=$1
 
 if [ -z "$CIRCLE_TOKEN" ]; then
     echo "CIRCLE_TOKEN environment variable must be set"
     exit
 fi
 
-if [ "$BUILD_NUMBER" = "latest" ]; then
+if [ "$JOB_NUMBER" = "latest" ]; then
     ARTIFACTS_RESPONSE=$(curl -s -H "Circle-Token: $CIRCLE_TOKEN" https://circleci.com/api/v1.1/project/github/murrple-1/rss_temple/latest/artifacts)
 else
-    ARTIFACTS_RESPONSE=$(curl -s -H "Circle-Token: $CIRCLE_TOKEN" "https://circleci.com/api/v1.1/project/github/murrple-1/rss_temple/$BUILD_NUMBER/artifacts")
+    ARTIFACTS_RESPONSE=$(curl -s -H "Circle-Token: $CIRCLE_TOKEN" "https://circleci.com/api/v1.1/project/github/murrple-1/rss_temple/$JOB_NUMBER/artifacts")
 fi
 
-echo $ARTIFACTS_RESPONSE | grep -P -o 'https://.*?build\.tar\.gz' | wget -q -O "build.tar.gz" --header "Circle-Token: $CIRCLE_TOKEN" --input-file -
+TARBALL_URL=$(echo $ARTIFACTS_RESPONSE | grep -P -m 1 -o 'https://.*?build\.tar\.gz')
 
-# tar -xzf build.tar.gz -C /var/www/rss_temple
+if [ -z "$TARBALL_URL" ]; then
+    echo "No tarball URL found in response"
+    exit
+fi
+
+wget -q -O "build.tar.gz" --header "Circle-Token: $CIRCLE_TOKEN" "$TARBALL_URL"
+
+mkdir -p /var/www/rss_temple/
+tar -xzf build.tar.gz -C /var/www/rss_temple/
