07a2cdd72b83f568427a17c19876a667d2acc05c
Murray Christopherson <murraychristopherson@gmail.com>
2021-03-25 13:49:12 -0700

    replaced html-sanitzer with bleach

--- rss_temple/api/content_sanitize.py
+++ rss_temple/api/content_sanitize.py
@@ -1,53 +1,111 @@
 import html
 import re
 
-import html_sanitizer
+import html5lib
+from html5lib.filters.base import Filter as HTML5LibFilter
+
+import bleach
 
 
-def _only_https_img(element):
-	if element.tag == 'img' and not element.attrib.get('src', '').startswith('https://'):
-		element.tag = 'img_'
+class TagRemovalFilter(HTML5LibFilter):
+    def __init__(self, *args, **kwargs):
+        self.tag = kwargs.pop('tag')
+        super().__init__(*args, **kwargs)
 
-	return element
+    def __iter__(self):
+        tag_depth = 0
+        for token in super().__iter__():
+            if token['type'] == 'StartTag' and token['name'] == self.tag:
+                tag_depth += 1
+            elif token['type'] == 'EndTag' and token['name'] == self.tag:
+                tag_depth -= 1
+            else:
+                if tag_depth <= 0:
+                    yield token
 
 
-_my_html_sanitizer_ = None
+class ScriptRemovalFiler(TagRemovalFilter):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, tag='script', **kwargs)
 
 
-def _my_html_sanitizer():
-    global _my_html_sanitizer_
-    if _my_html_sanitizer_ is None:
-        my_settings = dict(html_sanitizer.sanitizer.DEFAULT_SETTINGS)
+class StyleRemovalFiler(TagRemovalFilter):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, tag='style', **kwargs)
 
-        my_settings['tags'].add('img')
-        my_settings['tags'].add('pre')
-        my_settings['tags'].add('code')
-        my_settings['empty'].add('img')
-        my_settings['attributes'].update({'img': ('src', )})
-        my_settings['element_preprocessors'].append(_only_https_img)
 
-        _my_html_sanitizer_ = html_sanitizer.Sanitizer(settings=my_settings)
+class HTTPSOnlyImgFilter(HTML5LibFilter):
+    def __iter__(self):
+        for token in super().__iter__():
+            if token['type'] == 'EmptyTag' and token['name'] == 'img':
+                data = token['data']
+                if (None, 'src') in data and not data[(None, 'src')].startswith('https://'):
+                    continue
 
-    return _my_html_sanitizer_
+            yield token
+
+
+_my_bleach_filter_kwargs_ = None
+
+
+def _html_sanitizer_stream(source):
+    global _my_bleach_filter_kwargs_
+    if _my_bleach_filter_kwargs_ is None:
+        tags = set(bleach.sanitizer.ALLOWED_TAGS)
+        tags.add('p')
+        tags.add('img')
+        tags.add('br')
+
+        attributes = dict(bleach.sanitizer.ALLOWED_ATTRIBUTES)
+        attributes['img'] = ['src']
+
+        styles = set(bleach.sanitizer.ALLOWED_STYLES)
+
+        protocols = set(bleach.sanitizer.ALLOWED_PROTOCOLS)
+
+        _my_bleach_filter_kwargs_ = {
+            # see https://github.com/mozilla/bleach/blob/3e5d6aa375677821aaf249127e44ac51a815cf2b/bleach/sanitizer.py#L179
+            'attributes': attributes,
+            'strip_disallowed_elements': True,
+            'strip_html_comments': True,
+            # see https://github.com/mozilla/bleach/blob/3e5d6aa375677821aaf249127e44ac51a815cf2b/bleach/sanitizer.py#L183
+            'allowed_elements': tags,
+            'allowed_css_properties': styles,
+            'allowed_protocols': protocols,
+            'allowed_svg_properties': [],
+        }
+
+    filtered = ScriptRemovalFiler(source=source)
+    filtered = StyleRemovalFiler(source=filtered)
+    filtered = HTTPSOnlyImgFilter(source=filtered)
+    filtered = bleach.sanitizer.BleachSanitizerFilter(source=filtered, **_my_bleach_filter_kwargs_)
+
+    return filtered
 
 
 _is_html_regex = re.compile(r'<\/?[a-z][\s\S]*>', re.IGNORECASE)
 
 
 def is_html(text):
-	return _is_html_regex.search(text) is not None
+    return _is_html_regex.search(text) is not None
 
 
 def sanitize(text):
-	if is_html(text):
-		return sanitize_html(text)
-	else:
-		return sanitize_plain(text)
+    if is_html(text):
+        return sanitize_html(text)
+    else:
+        return sanitize_plain(text)
+
+
+_html_serializer = html5lib.serializer.HTMLSerializer()
 
 
 def sanitize_html(text):
-	return _my_html_sanitizer().sanitize(text)
+    dom = html5lib.parse(text, treebuilder='lxml')
+    walker = html5lib.getTreeWalker('lxml')
+    stream = _html_sanitizer_stream(walker(dom))
+    return _html_serializer.render(stream)
 
 
 def sanitize_plain(text):
-	return '<br>'.join(html.escape(text).splitlines())
+    return '<br>'.join(html.escape(text).splitlines())
--- rss_temple/api/tests/test_content_sanitizer.py
+++ rss_temple/api/tests/test_content_sanitizer.py
@@ -22,9 +22,38 @@ class ContentNormalizeTestCase(TestCase):
         self.assertEqual(content_sanitize.sanitize_html('Some Text<br />Some More Text'), 'Some Text<br>Some More Text')
 
         self.assertEqual(content_sanitize.sanitize_html('Some Text<br />Some More Text<script>console.log();</script>'), 'Some Text<br>Some More Text')
+        self.assertEqual(content_sanitize.sanitize_html('Some Text<br />Some More Text<script><script>console.log();</script></script>'), 'Some Text<br>Some More Text')
+        self.assertEqual(content_sanitize.sanitize_html('Some Text<br />Some More Text<style>>html {font-size: 50px;}</style>'), 'Some Text<br>Some More Text')
+        self.assertEqual(content_sanitize.sanitize_html('Some Text<br />Some More Text<style><style>html {font-size: 50px;}</style></style>'), 'Some Text<br>Some More Text')
+
+        self.assertEqual(content_sanitize.sanitize_html('<img src="https://test.com/something.png">'), '<img src="https://test.com/something.png">')
+        self.assertEqual(content_sanitize.sanitize_html('<img src="http://test.com/something.png">'), '')
+        self.assertEqual(content_sanitize.sanitize_html('<img src="smb://192.168.0.1/something.png">'), '')
 
     def test_sanitize_text(self):
         self.assertEqual(content_sanitize.sanitize_plain('Some Text'), 'Some Text')
         self.assertEqual(content_sanitize.sanitize_plain('Some Text\nSome More Text'), 'Some Text<br>Some More Text')
         self.assertEqual(content_sanitize.sanitize_plain('1 > 2'), '1 &gt; 2')
         self.assertEqual(content_sanitize.sanitize_plain('Some Text\nSome More Text 1 < 2'), 'Some Text<br>Some More Text 1 &lt; 2')
+        self.assertEqual(content_sanitize.sanitize_plain('<p>Some Text</p>'), '&lt;p&gt;Some Text&lt;/p&gt;')
+
+    def test_sanitize(self):
+        self.assertEqual(content_sanitize.sanitize('<p>Some Text</p>'), '<p>Some Text</p>')
+        self.assertEqual(content_sanitize.sanitize('<p>Some Text'), '<p>Some Text</p>')
+        self.assertEqual(content_sanitize.sanitize('Some Text<br>Some More Text'), 'Some Text<br>Some More Text')
+        self.assertEqual(content_sanitize.sanitize('Some Text<br/>Some More Text'), 'Some Text<br>Some More Text')
+        self.assertEqual(content_sanitize.sanitize('Some Text<br />Some More Text'), 'Some Text<br>Some More Text')
+
+        self.assertEqual(content_sanitize.sanitize('Some Text<br />Some More Text<script>console.log();</script>'), 'Some Text<br>Some More Text')
+        self.assertEqual(content_sanitize.sanitize('Some Text<br />Some More Text<script><script>console.log();</script></script>'), 'Some Text<br>Some More Text')
+        self.assertEqual(content_sanitize.sanitize('Some Text<br />Some More Text<style>>html {font-size: 50px;}</style>'), 'Some Text<br>Some More Text')
+        self.assertEqual(content_sanitize.sanitize('Some Text<br />Some More Text<style><style>html {font-size: 50px;}</style></style>'), 'Some Text<br>Some More Text')
+
+        self.assertEqual(content_sanitize.sanitize('<img src="https://test.com/something.png">'), '<img src="https://test.com/something.png">')
+        self.assertEqual(content_sanitize.sanitize('<img src="http://test.com/something.png">'), '')
+        self.assertEqual(content_sanitize.sanitize('<img src="smb://192.168.0.1/something.png">'), '')
+
+        self.assertEqual(content_sanitize.sanitize('Some Text'), 'Some Text')
+        self.assertEqual(content_sanitize.sanitize('Some Text\nSome More Text'), 'Some Text<br>Some More Text')
+        self.assertEqual(content_sanitize.sanitize('1 > 2'), '1 &gt; 2')
+        self.assertEqual(content_sanitize.sanitize('Some Text\nSome More Text 1 < 2'), 'Some Text<br>Some More Text 1 &lt; 2')
