4ce6e4eda7ac6c9da4bef39d9f888e98a3c4e84c
Murray Christopherson <murraychristopherson@gmail.com>
2021-03-31 22:21:33 -0700

    deduping the database before the new unique index

--- rss_temple/api/migrations/0006_auto_20210401_0502.py
+++ rss_temple/api/migrations/0006_auto_20210401_0502.py
@@ -2,6 +2,23 @@
 
 from django.db import migrations
 
+def _forward_func_deduplication_feed_entries(apps, schema_editor):
+    FeedEntry = apps.get_model('api', 'FeedEntry')
+
+    unique_set = set()
+    delete_list = []
+
+    for feed_entry in FeedEntry.objects.all():
+        unique_desc = (feed_entry.feed_id, feed_entry.url, feed_entry.updated_at)
+
+        if unique_desc in unique_set:
+            delete_list.append(feed_entry)
+        else:
+            unique_set.add(unique_desc)
+
+    for feed_entry in delete_list:
+        feed_entry.delete()
+
 
 class Migration(migrations.Migration):
 
@@ -10,6 +27,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(_forward_func_deduplication_feed_entries),
         migrations.RemoveIndex(
             model_name='feedentry',
             name='api_feedent_hash_5fbffa_idx',
