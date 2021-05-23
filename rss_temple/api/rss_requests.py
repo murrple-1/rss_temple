31be0bc66b7285af77f0d1f36933108528250d40
Murray Christopherson <murraychristopherson@gmail.com>
2021-05-23 15:24:35 -0700

    adding default timeout

--- rss_temple/api/rss_requests.py
+++ rss_temple/api/rss_requests.py
@@ -3,6 +3,6 @@ import requests
 
 def get(url, headers=None, *args, **kwargs):
     headers = headers or {}
-    return requests.get(url, headers={**headers, **{
+    return requests.get(url, timeout=30, headers={**headers, **{
         'User-Agent': 'RSS Temple',
     }}, *args, **kwargs)
