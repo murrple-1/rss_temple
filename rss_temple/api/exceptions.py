c5b7aa5839c309721be10fae6e6201cd9fc1fc2c
Murray Christopherson <murraychristopherson@gmail.com>
2022-03-17 13:30:50 -0700

    starting work converting to DRF

--- rss_temple/api/exceptions.py
+++
@@ -1,4 +0,0 @@
-class QueryException(Exception):
-    def __init__(self, message, httpcode):
-        self.message = message
-        self.httpcode = httpcode
--- rss_temple/rss_temple/urls.py
+++ rss_temple/rss_temple/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls')),
 ]
