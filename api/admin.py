101aba4896c7a615eb4672fced6cf2587429812a
Murray Christopherson <murraychristopherson@gmail.com>
2023-06-06 21:49:53 -0700

    fixing the fixture

--- api/admin.py
+++ api/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from api import models
 
+admin.site.register(models.User)
+admin.site.register(models.VerificationToken)
+admin.site.register(models.PasswordResetToken)
+admin.site.register(models.UserCategory)
 admin.site.register(models.Feed)
+admin.site.register(models.FeedEntry)
+admin.site.register(models.NotifyEmailQueueEntry)
