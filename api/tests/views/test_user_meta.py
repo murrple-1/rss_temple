4227973eb38a6ec3d7571cb51b0e6e95782b63b2
Murray Christopherson <murraychristopherson@gmail.com>
2023-09-08 11:29:55 -0700

    add test

---
+++ api/tests/views/test_user_meta.py
@@ -0,0 +1,49 @@
+import logging
+from typing import ClassVar
+
+from rest_framework.test import APITestCase
+
+from api.models import User
+
+
+class UserMetaTestCase(APITestCase):
+    old_app_logger_level: ClassVar[int]
+    old_django_logger_level: ClassVar[int]
+    user: ClassVar[User]
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls.old_app_logger_level = logging.getLogger("rss_temple").getEffectiveLevel()
+        cls.old_django_logger_level = logging.getLogger("django").getEffectiveLevel()
+
+        logging.getLogger("rss_temple").setLevel(logging.CRITICAL)
+        logging.getLogger("django").setLevel(logging.CRITICAL)
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+
+        logging.getLogger("rss_temple").setLevel(cls.old_app_logger_level)
+        logging.getLogger("django").setLevel(cls.old_django_logger_level)
+
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+
+        cls.user = User.objects.create_user("test@test.com", None)
+
+    def setUp(self):
+        super().setUp()
+
+        self.client.force_authenticate(user=UserMetaTestCase.user)
+
+    def test_UserCategoryView_get(self):
+        response = self.client.get(
+            f"/api/user/meta/readcount",
+        )
+        self.assertEqual(response.status_code, 200, response.content)
+
+        json_ = response.json()
+        self.assertIs(type(json_), int)
