89e4b77d3a01da3f44dbf69831f4e3ac64cb2a6f
Murray Christopherson <murraychristopherson@gmail.com>
2023-12-15 21:54:52 -0800

    more work to follow the RSS autodiscover spec

--- api/exposed_feed_extractor.py
+++ api/exposed_feed_extractor.py
@@ -1,10 +1,11 @@
 import logging
 import pprint
 from dataclasses import dataclass
+from urllib.parse import urljoin, urlparse
 
 import feedparser
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, ResultSet, Tag
 
 from api import rss_requests
 
@@ -46,16 +47,36 @@ def extract_exposed_feeds(
     # TODO investigate what errors this can throw (if any), and handle them
     soup = BeautifulSoup(response.text, "lxml")
 
-    rss_links = soup.findAll("link", type="application/rss+xml")
+    base_href: str | None = None
+    if (
+        isinstance((base_tag := soup.find("base")), Tag)
+        and (base_href_ := base_tag.get("href"))
+        and isinstance(base_href_, str)
+    ):
+        base_href = base_href_
+    else:
+        base_href = url
+
+    rss_links: ResultSet[Tag] = soup.findAll(
+        "link", rel="alternate", type="application/rss+xml"
+    )
 
     exposed_feeds: list[ExposedFeed] = []
     for rss_link in rss_links:
         href = rss_link.get("href")
-        if not href:
+        if not href or not isinstance(href, str):
+            continue
+
+        href = urljoin(base_href, href)
+
+        if (href_parse := urlparse(href)).scheme not in (
+            "http",
+            "https",
+        ) or not href_parse.netloc:
             continue
 
         title = rss_link.get("title")
-        if not title:
+        if not title or not isinstance(title, str):
             title = href
 
         exposed_feeds.append(ExposedFeed(title, href))
--- api/tests/test_extract_exposed_feeds.py
+++ api/tests/test_extract_exposed_feeds.py
@@ -30,15 +30,35 @@ class ExtractExposedFeedsTestCase(TestFileServerTestCase):
 
     @tag("slow")
     def test_extract_exposed_feeds(self):
-        exposed_feeds = extract_exposed_feeds(
-            f"{ExtractExposedFeedsTestCase.live_server_url}/site/single_exposed_feed.html"
-        )
-        self.assertEqual(len(exposed_feeds), 1)
-
-        exposed_feeds = extract_exposed_feeds(
-            f"{ExtractExposedFeedsTestCase.live_server_url}/site/multi_exposed_feeds.html"
-        )
-        self.assertEqual(len(exposed_feeds), 3)
+        for url, expected_feed_count in [
+            (
+                f"{ExtractExposedFeedsTestCase.live_server_url}/site/exposed_feeds/single.html",
+                1,
+            ),
+            (
+                f"{ExtractExposedFeedsTestCase.live_server_url}/site/exposed_feeds/multi.html",
+                3,
+            ),
+            (
+                f"{ExtractExposedFeedsTestCase.live_server_url}/site/exposed_feeds/relative.html",
+                1,
+            ),
+            (
+                f"{ExtractExposedFeedsTestCase.live_server_url}/site/exposed_feeds/base_absolute.html",
+                1,
+            ),
+            (
+                f"{ExtractExposedFeedsTestCase.live_server_url}/site/exposed_feeds/base_relative.html",
+                1,
+            ),
+            (
+                f"{ExtractExposedFeedsTestCase.live_server_url}/site/exposed_feeds/bad_base_relative.html",
+                0,
+            ),
+        ]:
+            with self.subTest(url=url):
+                exposed_feeds = extract_exposed_feeds(url)
+                self.assertEqual(len(exposed_feeds), expected_feed_count)
 
     @tag("slow")
     def test_extract_exposed_feeds_justafeed(self):
