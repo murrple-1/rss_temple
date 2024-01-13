aff2eeaf0f3302dc4ec0e19c3ac1556ef9eeb6e7
Murray Christopherson <murraychristopherson@gmail.com>
2024-01-13 13:21:19 -0800

    adding another temp-hack to help find the OOM error

--- api_dramatiq/middleware/log.py
+++ api_dramatiq/middleware/log.py
@@ -1,7 +1,16 @@
+import os
 from typing import Any
 
 from dramatiq import Broker, Message, get_logger
 from dramatiq.middleware import Middleware
+
+
+def _message_str(message: Message) -> str:
+    return f"{message.actor_name}|{message.message_id}|{message.message_timestamp}"
+
+
+def _message_lock_filepath(message: Message) -> str:
+    return os.path.join("mount/", f"{message.message_id}.lock")
 
 
 class BeforeAfterLog(Middleware):
@@ -11,7 +20,12 @@ class BeforeAfterLog(Middleware):
         self.logger = get_logger(__name__, type(self))
 
     def before_process_message(self, broker: Broker, message: Message):
-        message_str = str(message)
+        message_str = _message_str(message)
+
+        # TODO remove, this is a temp hack to find a hard-to-trace bug
+        with open(_message_lock_filepath(message), "w") as f:
+            f.write(message_str)
+
         self.logger.info("before message: %s", message_str)
 
     def after_process_message(
@@ -22,4 +36,7 @@ class BeforeAfterLog(Middleware):
         result: Any = None,
         exception: BaseException | None = None,
     ):
-        self.logger.info("after message: %s", message)
+        # TODO remove, this is a temp hack to find a hard-to-trace bug
+        os.unlink(_message_lock_filepath(message))
+
+        self.logger.info("after message: %s", _message_str(message))
