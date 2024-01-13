26c067ade6cbd0dbc81414250a14e7fda77fac49
Murray Christopherson <murraychristopherson@gmail.com>
2024-01-13 11:50:33 -0800

    adding another temp-hack to help find the OOM error

--- api_dramatiq/middleware/log.py
+++ api_dramatiq/middleware/log.py
@@ -1,3 +1,5 @@
+import os
+import uuid
 from typing import Any
 
 from dramatiq import Broker, Message, get_logger
@@ -9,9 +11,15 @@ class BeforeAfterLog(Middleware):
         super().__init__(*args, **kwargs)
 
         self.logger = get_logger(__name__, type(self))
+        # TODO remove, this is a temp hack to find a hard-to-trace bug
+        self.lock_filepath = os.path.join("mount/", f"{uuid.uuid4()}.lock")
 
     def before_process_message(self, broker: Broker, message: Message):
-        self.logger.info("before message: %s", message)
+        message_str = str(message)
+        with open(self.lock_filepath, "w") as f:
+            f.write(message_str)
+
+        self.logger.info("before message: %s", message_str)
 
     def after_process_message(
         self,
@@ -19,6 +27,7 @@ class BeforeAfterLog(Middleware):
         message: Message,
         *,
         result: Any = None,
-        exception: BaseException | None = None
+        exception: BaseException | None = None,
     ):
+        os.unlink(self.lock_filepath)
         self.logger.info("after message: %s", message)
