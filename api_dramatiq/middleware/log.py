1880df74bacd0bb76f5839a6d50c8ab2dd6843c8
Murray Christopherson <murraychristopherson@gmail.com>
2024-01-13 13:11:40 -0800

    need a new tactic, didn't work as I'd hoped

--- api_dramatiq/middleware/log.py
+++ api_dramatiq/middleware/log.py
@@ -1,5 +1,3 @@
-import os
-import uuid
 from typing import Any
 
 from dramatiq import Broker, Message, get_logger
@@ -11,14 +9,9 @@ class BeforeAfterLog(Middleware):
         super().__init__(*args, **kwargs)
 
         self.logger = get_logger(__name__, type(self))
-        # TODO remove, this is a temp hack to find a hard-to-trace bug
-        self.lock_filepath = os.path.join("mount/", f"{uuid.uuid4()}.lock")
 
     def before_process_message(self, broker: Broker, message: Message):
         message_str = str(message)
-        with open(self.lock_filepath, "w") as f:
-            f.write(message_str)
-
         self.logger.info("before message: %s", message_str)
 
     def after_process_message(
@@ -29,5 +22,4 @@ class BeforeAfterLog(Middleware):
         result: Any = None,
         exception: BaseException | None = None,
     ):
-        os.unlink(self.lock_filepath)
         self.logger.info("after message: %s", message)
