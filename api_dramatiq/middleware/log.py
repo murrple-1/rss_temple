6eedbe344896db588d31986614476c8bd4710b85
Murray Christopherson <murraychristopherson@gmail.com>
2024-01-13 16:41:38 -0800

    now doing some memory tracing

--- api_dramatiq/middleware/log.py
+++ api_dramatiq/middleware/log.py
@@ -5,6 +5,10 @@ from dramatiq import Broker, Message, get_logger
 from dramatiq.middleware import Middleware
 
 
+def _message_str(message: Message) -> str:
+    return f"{message.actor_name}|{message.message_id}|{message.message_timestamp}"
+
+
 class BeforeAfterLog(Middleware):
     snapshots: dict[str, tracemalloc.Snapshot]
 
@@ -16,6 +20,8 @@ class BeforeAfterLog(Middleware):
         tracemalloc.start()
 
     def before_process_message(self, broker: Broker, message: Message):
+        self.logger.info("before message: %s", _message_str(message))
+
         self.snapshots[message.message_id] = tracemalloc.take_snapshot()
 
     def after_process_message(
@@ -26,10 +32,13 @@ class BeforeAfterLog(Middleware):
         result: Any = None,
         exception: BaseException | None = None,
     ):
+        self.logger.info("after message: %s", _message_str(message))
+
         old_snapshot = self.snapshots.pop(message.message_id)
         final_snapshot = tracemalloc.take_snapshot()
 
         top_stats = final_snapshot.compare_to(old_snapshot, "lineno")
 
+        self.logger.info("top 10 differences")
         for stat in top_stats[:10]:
             self.logger.info(str(stat))
