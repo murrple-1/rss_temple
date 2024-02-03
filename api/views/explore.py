71daba55ef66541cd60415d35053882511aa651f
Murray Christopherson <murraychristopherson@gmail.com>
2024-02-02 22:59:01 -0800

    small improvement to explore endpoint for now

--- api/views/explore.py
+++ api/views/explore.py
@@ -32,132 +32,137 @@ TODO: for the time being, this will just be static data (based on my personal OP
     def get(self, request: Request):
         cache: BaseCache = caches["default"]
 
-        cache_key = "explore__ret_ob"
-        ret_obj: list[dict[str, Any]] | None = cache.get(cache_key)
-        cache_hit = True
-        if not ret_obj:
-            section_lookups: list[_Section] = [
-                {
-                    "tag": "Gaming",
-                    "feeds": [
-                        {
-                            "feed_url": "http://feeds.feedburner.com/GamasutraFeatureArticles",
-                            "image_src": "/assets/images/explore_banner.png",
-                        },
-                        {
-                            "feed_url": "http://feeds.wolfire.com/WolfireGames",
-                            "image_src": "/assets/images/explore_banner.png",
-                        },
-                    ],
-                },
-                {
-                    "tag": "Technology",
-                    "feeds": [
-                        {
-                            "feed_url": "http://rss.slashdot.org/Slashdot/slashdot",
-                            "image_src": None,
-                        },
-                        {
-                            "feed_url": "http://feeds.arstechnica.com/arstechnica/index",
-                            "image_src": None,
-                        },
-                    ],
-                },
-                {
-                    "tag": "World News",
-                    "feeds": [
-                        {
-                            "feed_url": "https://www.ctvnews.ca/rss/ctvnews-ca-top-stories-public-rss-1.822009",
-                            "image_src": None,
-                        },
-                    ],
-                },
-                {
-                    "tag": "Programming",
-                    "feeds": [
-                        {
-                            "feed_url": "http://feeds.feedburner.com/codinghorror",
-                            "image_src": None,
-                        },
-                        {
-                            "feed_url": "http://feeds.wolfire.com/WolfireGames",
-                            "image_src": None,
-                        },
-                        {
-                            "feed_url": "http://syndication.thedailywtf.com/TheDailyWtf",
-                            "image_src": None,
-                        },
-                    ],
-                },
-                {
-                    "tag": "Music",
-                    "feeds": [
-                        {
-                            "feed_url": "http://battlehelm.com/feed/",
-                            "image_src": "/assets/images/explore_banner.png",
-                        },
-                        {
-                            "feed_url": "http://www.theblackplanet.org/feed/",
-                            "image_src": "/assets/images/explore_banner.png",
-                        },
-                        {
-                            "feed_url": "http://www.angrymetalguy.com/feed/",
-                            "image_src": "/assets/images/explore_banner.png",
-                        },
-                        {
-                            "feed_url": "http://www.terrorizer.com/feed/",
-                            "image_src": "/assets/images/explore_banner.png",
-                        },
-                        {
-                            "feed_url": "http://deadrhetoric.com/feed/",
-                            "image_src": "/assets/images/explore_banner.png",
-                        },
-                    ],
-                },
-            ]
+        section_lookups: list[_Section] = [
+            {
+                "tag": "Gaming",
+                "feeds": [
+                    {
+                        "feed_url": "http://feeds.feedburner.com/GamasutraFeatureArticles",
+                        "image_src": "/assets/images/explore_banner.png",
+                    },
+                    {
+                        "feed_url": "http://feeds.wolfire.com/WolfireGames",
+                        "image_src": "/assets/images/explore_banner.png",
+                    },
+                ],
+            },
+            {
+                "tag": "Technology",
+                "feeds": [
+                    {
+                        "feed_url": "http://rss.slashdot.org/Slashdot/slashdot",
+                        "image_src": None,
+                    },
+                    {
+                        "feed_url": "http://feeds.arstechnica.com/arstechnica/index",
+                        "image_src": None,
+                    },
+                ],
+            },
+            {
+                "tag": "World News",
+                "feeds": [
+                    {
+                        "feed_url": "https://www.ctvnews.ca/rss/ctvnews-ca-top-stories-public-rss-1.822009",
+                        "image_src": None,
+                    },
+                ],
+            },
+            {
+                "tag": "Programming",
+                "feeds": [
+                    {
+                        "feed_url": "http://feeds.feedburner.com/codinghorror",
+                        "image_src": None,
+                    },
+                    {
+                        "feed_url": "http://feeds.wolfire.com/WolfireGames",
+                        "image_src": None,
+                    },
+                    {
+                        "feed_url": "http://syndication.thedailywtf.com/TheDailyWtf",
+                        "image_src": None,
+                    },
+                ],
+            },
+            {
+                "tag": "Music",
+                "feeds": [
+                    {
+                        "feed_url": "http://battlehelm.com/feed/",
+                        "image_src": "/assets/images/explore_banner.png",
+                    },
+                    {
+                        "feed_url": "http://www.theblackplanet.org/feed/",
+                        "image_src": "/assets/images/explore_banner.png",
+                    },
+                    {
+                        "feed_url": "http://www.angrymetalguy.com/feed/",
+                        "image_src": "/assets/images/explore_banner.png",
+                    },
+                    {
+                        "feed_url": "http://www.terrorizer.com/feed/",
+                        "image_src": "/assets/images/explore_banner.png",
+                    },
+                    {
+                        "feed_url": "http://deadrhetoric.com/feed/",
+                        "image_src": "/assets/images/explore_banner.png",
+                    },
+                ],
+            },
+        ]
 
-            ret_obj = []
-            for section_lookup in section_lookups:
-                feed_objs: list[dict[str, Any]] = []
-                for feed_lookup in section_lookup["feeds"]:
-                    feed: Feed
-                    try:
-                        feed = Feed.annotate_subscription_data(
-                            Feed.objects.all(), cast(User, request.user)
-                        ).get(feed_url=feed_lookup["feed_url"])
-                    except Feed.DoesNotExist:
-                        continue
+        ret_obj: list[dict[str, Any]] = []
 
-                    some_feed_entries = list(
+        feeds = {
+            f.feed_url: f
+            for f in Feed.annotate_subscription_data(
+                Feed.objects.filter(
+                    feed_url__in=(
+                        sf["feed_url"] for s in section_lookups for sf in s["feeds"]
+                    )
+                ),
+                cast(User, request.user),
+            )
+        }
+
+        for section_lookup in section_lookups:
+            feed_objs: list[dict[str, Any]] = []
+            for feed_lookup in section_lookup["feeds"]:
+                feed = feeds.get(feed_lookup["feed_url"])
+                if not feed:
+                    continue
+
+                cache_key = f"explore__some_feed_entry_titles__{feed.uuid}"
+                some_feed_entry_titles: list[str] | None = cache.get(cache_key)
+                if some_feed_entry_titles is None:
+                    some_feed_entry_titles = list(
                         FeedEntry.objects.filter(feed=feed, title__isnull=False)
                         .order_by("published_at")
                         .values_list("title", flat=True)[:5]
                     )
-                    if len(some_feed_entries) < 1:
-                        continue
+                    cache.set(cache_key, some_feed_entry_titles, 60.0 * 60.0 * 24.0)
 
-                    feed_objs.append(
-                        {
-                            "name": feed.title,
-                            "feedUrl": feed.feed_url,
-                            "homeUrl": feed.home_url,
-                            "imageSrc": feed_lookup["image_src"],
-                            "entryTitles": some_feed_entries,
-                            "isSubscribed": feed.is_subscribed,
-                        }
-                    )
+                if len(some_feed_entry_titles) < 1:
+                    continue
 
-                if len(feed_objs) > 0:
-                    ret_obj.append(
-                        {
-                            "tagName": section_lookup["tag"],
-                            "feeds": feed_objs,
-                        }
-                    )
+                feed_objs.append(
+                    {
+                        "name": feed.title,
+                        "feedUrl": feed.feed_url,
+                        "homeUrl": feed.home_url,
+                        "imageSrc": feed_lookup["image_src"],
+                        "entryTitles": some_feed_entry_titles,
+                        "isSubscribed": feed.is_subscribed,
+                    }
+                )
 
-            cache_hit = False
-            cache.set(cache_key, ret_obj, 60.0 * 60.0 * 24.0 * 7.0)
+            if len(feed_objs) > 0:
+                ret_obj.append(
+                    {
+                        "tagName": section_lookup["tag"],
+                        "feeds": feed_objs,
+                    }
+                )
 
-        response = Response(ExploreSerializer(ret_obj, many=True).data)
-        response["X-Cache-Hit"] = "YES" if cache_hit else "NO"
-        return response
+        return Response(ExploreSerializer(ret_obj, many=True).data)
