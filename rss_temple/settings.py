aaed03cb9f889307c45c8bc470a57a6e386d9c46
Murray Christopherson <murraychristopherson@gmail.com>
2024-02-23 17:28:58 -0800

    remove comment

--- rss_temple/settings.py
+++ rss_temple/settings.py
@@ -416,7 +416,6 @@ LABELING_EXPIRY_INTERVAL = datetime.timedelta(days=7)
 
 EXPOSED_FEEDS_CACHE_TIMEOUT_SECONDS: float | None = 60.0 * 60.0 * 12.0  # 12 hours
 
-# experimental
 FEED_COUNTS_LOOKUP_COMBINED_QUERYSET_MIN_COUNT = 256
 
 try:
