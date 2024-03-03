11e4f5f832fc86cf4a0075a8177c23998a2cbb2f
Murray Christopherson <murraychristopherson@gmail.com>
2024-03-02 16:45:51 -0800

    explore uses lock now too

--- api/views/explore.py
+++ api/views/explore.py
@@ -3,6 +3,7 @@ from typing import Any, TypedDict, cast
 from django.core.cache import BaseCache, caches
 from django.db.models import Q
 from drf_yasg.utils import swagger_auto_schema
+from redis_lock.django_cache import RedisCache as RedisLockCache
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -115,76 +116,88 @@ TODO: for the time being, this will just be static data (based on my personal OP
     def get(self, request: Request):
         cache: BaseCache = caches["default"]
 
-        cache_key = "explore__ret_obj"
-        ret_obj: list[dict[str, Any]] | None = cache.get(cache_key)
-        cache_hit = True
-        if ret_obj is None:
-            ret_obj = []
-
-            feeds: dict[str, Feed] = {}
-            for s in _section_lookups:
-                for sf in s["feeds"]:
-                    url = cast(str, url_normalize(sf["feed_url"]))
-                    feed = Feed.objects.filter(
-                        Q(feed_url=url)
-                        | Q(
-                            uuid__in=AlternateFeedURL.objects.filter(
-                                feed_url=url
-                            ).values("feed_id")[:1]
-                        )
-                    ).first()
-                    if feed is not None:
-                        feeds[url] = feed
-
-            for section_lookup in _section_lookups:
-                feed_objs: list[dict[str, Any]] = []
-                for feed_lookup in section_lookup["feeds"]:
-                    feed = feeds.get(feed_lookup["feed_url"])
-                    if not feed:
-                        continue
-
-                    some_feed_entry_titles = list(
-                        feed.feed_entries.filter(is_archived=False)
-                        .order_by("-published_at")
-                        .values_list("title", flat=True)[:5]
-                    )
-
-                    if not some_feed_entry_titles:
-                        continue
-
-                    feed_objs.append(
-                        {
-                            "_uuid": feed.uuid,
-                            "name": feed.title,
-                            "feedUrl": feed.feed_url,
-                            "homeUrl": feed.home_url,
-                            "imageSrc": feed_lookup["image_src"],
-                            "entryTitles": some_feed_entry_titles,
-                        }
-                    )
-
-                if feed_objs:
-                    ret_obj.append(
-                        {
-                            "tagName": section_lookup["tag"],
-                            "feeds": feed_objs,
-                        }
-                    )
-
-            cache_hit = False
-            cache.set(cache_key, ret_obj, 60.0 * 60.0 * 3.0)
-
-        subscribed_feed_uuids = frozenset(
-            SubscribedFeedUserMapping.objects.filter(
-                user=cast(User, request.user)
-            ).values_list("feed_id", flat=True)
+        lock = (
+            cache.lock("explore_lock", expire=60, auto_renewal=True)
+            if isinstance(cache, RedisLockCache)
+            else None
         )
+        if lock is not None:
+            lock.acquire()
 
-        for json_ in ret_obj:
-            for feed_json in json_["feeds"]:
-                uuid_ = feed_json.pop("_uuid")
-                feed_json["isSubscribed"] = uuid_ in subscribed_feed_uuids
+        try:
+            cache_key = "explore__ret_obj"
+            ret_obj: list[dict[str, Any]] | None = cache.get(cache_key)
+            cache_hit = True
+            if ret_obj is None:
+                ret_obj = []
 
-        response = Response(ExploreSerializer(ret_obj, many=True).data)
-        response["X-Cache-Hit"] = "YES" if cache_hit else "NO"
-        return response
+                feeds: dict[str, Feed] = {}
+                for s in _section_lookups:
+                    for sf in s["feeds"]:
+                        url = cast(str, url_normalize(sf["feed_url"]))
+                        feed = Feed.objects.filter(
+                            Q(feed_url=url)
+                            | Q(
+                                uuid__in=AlternateFeedURL.objects.filter(
+                                    feed_url=url
+                                ).values("feed_id")[:1]
+                            )
+                        ).first()
+                        if feed is not None:
+                            feeds[url] = feed
+
+                for section_lookup in _section_lookups:
+                    feed_objs: list[dict[str, Any]] = []
+                    for feed_lookup in section_lookup["feeds"]:
+                        feed = feeds.get(feed_lookup["feed_url"])
+                        if not feed:
+                            continue
+
+                        some_feed_entry_titles = list(
+                            feed.feed_entries.filter(is_archived=False)
+                            .order_by("-published_at")
+                            .values_list("title", flat=True)[:5]
+                        )
+
+                        if not some_feed_entry_titles:
+                            continue
+
+                        feed_objs.append(
+                            {
+                                "_uuid": feed.uuid,
+                                "name": feed.title,
+                                "feedUrl": feed.feed_url,
+                                "homeUrl": feed.home_url,
+                                "imageSrc": feed_lookup["image_src"],
+                                "entryTitles": some_feed_entry_titles,
+                            }
+                        )
+
+                    if feed_objs:
+                        ret_obj.append(
+                            {
+                                "tagName": section_lookup["tag"],
+                                "feeds": feed_objs,
+                            }
+                        )
+
+                cache_hit = False
+                cast(BaseCache, cache).set(cache_key, ret_obj, 60.0 * 60.0 * 3.0)
+
+            subscribed_feed_uuids = frozenset(
+                SubscribedFeedUserMapping.objects.filter(
+                    user=cast(User, request.user)
+                ).values_list("feed_id", flat=True)
+            )
+
+            for json_ in ret_obj:
+                for feed_json in json_["feeds"]:
+                    uuid_ = feed_json.pop("_uuid")
+                    feed_json["isSubscribed"] = uuid_ in subscribed_feed_uuids
+
+            response = Response(ExploreSerializer(ret_obj, many=True).data)
+            response["X-Cache-Hit"] = "YES" if cache_hit else "NO"
+            return response
+        finally:
+            if lock is not None:
+                lock.release()
