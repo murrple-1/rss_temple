50795a169feca6a7cfc1b684822eb1c8667cf4a9
Murray Christopherson <murraychristopherson@gmail.com>
2024-03-04 15:57:21 -0800

    an optimization on classifier label vote counting

--- api/cache_utils/classifier_label_vote_counts.py
+++ api/cache_utils/classifier_label_vote_counts.py
@@ -1,19 +1,14 @@
 import uuid as uuid_
-from typing import Any, Collection
+from collections import defaultdict
+from typing import Any, Collection, Generator
 
 from django.conf import settings
 from django.core.cache import BaseCache
 from django.core.signals import setting_changed
-from django.db.models import Count, OuterRef, Subquery
-from django.db.models.functions import Coalesce
+from django.db import connection
 from django.dispatch import receiver
 
-from api.lock_context import lock_context
-from api.models import (
-    ClassifierLabel,
-    ClassifierLabelFeedEntryCalculated,
-    ClassifierLabelFeedEntryVote,
-)
+from api.models import ClassifierLabel
 
 _CLASSIFIER_LABEL_VOTE_COUNTS_CACHE_TIMEOUT_SECONDS: float | None
 
@@ -30,57 +25,100 @@ def _load_global_settings(*args: Any, **kwargs: Any):
 _load_global_settings()
 
 
+def _generate_cached_entries(
+    feed_entry_uuids: Collection[uuid_.UUID], cache: BaseCache
+) -> Generator[tuple[uuid_.UUID, dict[uuid_.UUID, int]], None, None]:
+    cache_entries: dict[str, dict[uuid_.UUID, int] | None] = cache.get_many(
+        f"classifier_label_vote_counts__{fe_uuid}" for fe_uuid in feed_entry_uuids
+    )
+
+    for key, entry in cache_entries.items():
+        if entry is not None:
+            feed_entry_uuid = uuid_.UUID(
+                key.removeprefix("classifier_label_vote_counts__")
+            )
+            yield feed_entry_uuid, entry
+
+
 def get_classifier_label_vote_counts_from_cache(
     feed_entry_uuids: Collection[uuid_.UUID], cache: BaseCache
 ) -> tuple[dict[uuid_.UUID, dict[uuid_.UUID, int]], bool]:
-    classifier_label_vote_counts: dict[uuid_.UUID, dict[uuid_.UUID, int]] = {}
+    feed_entry_uuids = frozenset(feed_entry_uuids)
+
+    classifier_label_vote_counts: dict[uuid_.UUID, dict[uuid_.UUID, int]] = {
+        feed_entry_uuid: entry
+        for feed_entry_uuid, entry in _generate_cached_entries(feed_entry_uuids, cache)
+    }
     cache_hit = True
 
-    for feed_entry_uuid in feed_entry_uuids:
-        with lock_context(
-            cache, f"classifier_label_vote_counts_lock__{feed_entry_uuid}"
-        ):
-            cache_key = f"classifier_label_vote_counts__{feed_entry_uuid}"
-            vote_counts: dict[uuid_.UUID, int] | None = cache.get(cache_key)
+    missing_feed_entry_uuids = feed_entry_uuids.difference(
+        classifier_label_vote_counts.keys()
+    )
+    if missing_feed_entry_uuids:
+        placeholders = ", ".join("%s" for _ in missing_feed_entry_uuids)
+        params: tuple[Any, ...]
+        if connection.vendor == "sqlite":
+            params = tuple(str(u).replace("-", "") for u in missing_feed_entry_uuids)
+        else:
+            params = tuple(missing_feed_entry_uuids)
 
-            if vote_counts is None:
-                vote_counts = {
-                    d["uuid"]: d["vote_count"]
-                    for d in ClassifierLabel.objects.annotate(
-                        vote_count=Coalesce(
-                            Subquery(
-                                ClassifierLabelFeedEntryVote.objects.filter(
-                                    feed_entry_id=feed_entry_uuid,
-                                    classifier_label_id=OuterRef("uuid"),
-                                )
-                                .values("feed_entry")
-                                .annotate(c1=Count("uuid"))
-                                .values("c1")
-                            ),
-                            0,
-                        )
-                        + Coalesce(
-                            Subquery(
-                                ClassifierLabelFeedEntryCalculated.objects.filter(
-                                    feed_entry_id=feed_entry_uuid,
-                                    classifier_label_id=OuterRef("uuid"),
-                                )
-                                .values("feed_entry")
-                                .annotate(c2=Count("uuid"))
-                                .values("c2")
-                            ),
-                            0,
-                        ),
-                    ).values("uuid", "vote_count")
-                }
-
-                cache.set(
-                    cache_key,
-                    vote_counts,
-                    _CLASSIFIER_LABEL_VOTE_COUNTS_CACHE_TIMEOUT_SECONDS,
+        rows = ClassifierLabel.objects.raw(
+            f"""
+            SELECT
+            t1."uuid" AS "uuid",
+            t2."uuid" AS "feed_entry_uuid",
+            (
+                (
+                    SELECT
+                        COUNT(*)
+                    FROM
+                        api_classifierlabelfeedentryvote AS u1
+                    WHERE
+                        u1."classifier_label_id" = t1."uuid"
+                        AND u1."feed_entry_id" = t2."uuid"
+            ) + (
+                    SELECT
+                        COUNT(*)
+                    FROM
+                        api_classifierlabelfeedentrycalculated AS u2
+                    WHERE
+                        u2."classifier_label_id" = t1."uuid"
+                        AND u2."feed_entry_id" = t2."uuid"
                 )
-                cache_hit = False
+            ) AS "vote_count"
+            FROM
+                api_classifierlabel AS t1
+                JOIN api_feedentry AS t2 ON TRUE
+            WHERE
+                t2."uuid" IN ({placeholders})
+            GROUP BY
+                t1."uuid",
+                t2."uuid"
+        """,
+            params,
+        )
 
-        classifier_label_vote_counts[feed_entry_uuid] = vote_counts
+        missing_classifier_label_vote_counts: dict[
+            uuid_.UUID, dict[uuid_.UUID, int]
+        ] = defaultdict(dict)
+        for row in rows:
+            feed_entry_uuid = row.feed_entry_uuid
+            if not isinstance(feed_entry_uuid, uuid_.UUID):
+                feed_entry_uuid = uuid_.UUID(feed_entry_uuid)
+
+            missing_classifier_label_vote_counts[feed_entry_uuid][
+                row.uuid
+            ] = row.vote_count
+
+        classifier_label_vote_counts.update(missing_classifier_label_vote_counts)
+
+        cache.set_many(
+            {
+                f"classifier_label_vote_counts__{feed_entry_uuid}": vote_counts
+                for feed_entry_uuid, vote_counts in missing_classifier_label_vote_counts.items()
+            },
+            _CLASSIFIER_LABEL_VOTE_COUNTS_CACHE_TIMEOUT_SECONDS,
+        )
+        cache_hit = False
 
     return classifier_label_vote_counts, cache_hit
