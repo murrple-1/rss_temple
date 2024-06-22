3dff600e13958b281c918cd6071baadec175f088
Murray Christopherson <murraychristopherson@gmail.com>
2024-06-21 23:00:24 -0700

    deferred attribute monkey patch no longer necessary

--- api/apps.py
+++ api/apps.py
@@ -3,10 +3,3 @@ from django.apps import AppConfig
 
 class ApiConfig(AppConfig):
     name = "api"
-
-    def ready(self) -> None:
-        from api.deferredattribute_monkey_patch import monkey_patch
-
-        monkey_patch()
-
-        return super().ready()
